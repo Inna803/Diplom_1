354c3b8561950575df0bd4fb95f9f448efbd7237
Inna Malysheva <innuchca.malysheva@yandex.ru>
2023-06-22 09:41:31 +0300

    исправление тестового класса BunTest (добавление параметризации)

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -2,31 +2,65 @@ package praktikum;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    //передоваемое тестовое имя и цена
-    private final String EXPECTED_NAME = "test bun";
-    private final float EXPECTED_PRICE = 6.66F;
-    private Bun bun;
+    // переменные для хранения параметров тестовых данных
+    private final String bunName;
+    private final float bunPrice;
+
+    private Bun bun; // объект Bun для тестирования
+
+    // конструктор класса, принимающий параметры тестовых данных
+    public BunTest(String bunName, float bunPrice) {
+        this.bunName = bunName;
+        this.bunPrice = bunPrice;
+    }
+
+    // метод, возвращающий коллекцию массивов объектов с тестовыми данными
+    @Parameterized.Parameters(name = "Name: {0}, Price: {1}")
+    public static Collection<Object[]> getTestData() {
+        return Arrays.asList(new Object[][]{
+                {"Обычная", 1},      // Обычная булочка с ценой 1
+                {"Вкусная", 20},     // Вкусная булочка с ценой 20
+                {"Съедобная", 300},  // Съедобная булочка с ценой 300
+                {"Флот", 0.01F},     // булочка "Флот" с ценой 0.01
+                {"Бесценная", 0},    // булочка "Бесценная" с ценой 0
+                {" ", 0},            // булочка с пустым именем и ценой 0
+                {"", 0},             // булочка с пустым именем и ценой 0
+                {null, 0},           // булочка с нулевым именем и ценой 0
+                {"Отрицательная", -100},  // булочка "Отрицательная" с отрицательной ценой -100
+                {"Цифровая_1234", 1234},  // булочка "Цифровая_1234" с ценой 1234
+                {"$%#^@&$$(%^^)символьная", 0},   // булочка с символами в имени и ценой 0
+                {"ОченьОченьОченьДлинноеИмяДляБулочки", Float.MAX_VALUE}   // булочка с очень длинным именем и максимальной ценой
+        });
+    }
 
     @Before
-    public void setUp(){
-        bun = new Bun(EXPECTED_NAME, EXPECTED_PRICE);
+    public void setUp() {
+        // создаем новый объект Bun с заданными именем и ценой перед каждым тестом
+        bun = new Bun(bunName, bunPrice);
     }
 
-    //проверяем, что метод getName возвращает имя указанное в конструкторе
     @Test
-    public void getNameOfBun() {
+    public void testGetName() {
+        // проверяем, что метод getName() возвращает ожидаемое имя
         String actual = bun.getName();
-        assertEquals("The name of the bun does not match the specified!", actual, EXPECTED_NAME);
+        assertEquals("The name of the bun does not match the specified!", bunName, actual);
     }
-    //проверяем, что метод getPrice возвращает цену указанную в конструкторе
+
     @Test
-    public void getPriceOfBun() {
+    public void testGetPrice() {
+        // проверяем, что метод getPrice() возвращает ожидаемую цену
         float actual = bun.getPrice();
-        assertEquals("The price of the bun does not match the specified!", actual, EXPECTED_PRICE, 0);
+        assertEquals("The price of the bun does not match the specified!", bunPrice, actual, 0);
     }
 }
